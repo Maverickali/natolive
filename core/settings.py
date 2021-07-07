571c9bc810270d8484a63e119866ec6f88a0f15c
Collins Maverick <collinsmaverick11@gmail.com>
2021-07-07 16:11:34 +0300

    setting file updated

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,160 @@
+# # -*- encoding: utf-8 -*-
+
+# import os
+# from decouple import config
+# from unipath import Path
+# import dj_database_url
+
+# # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# BASE_DIR    = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# PROJECT_DIR = Path(__file__).parent
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
+
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = config('DEBUG', default=False)
+
+# # load production server from .env
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '102.83.76.169', config('SERVER', default='127.0.0.1')]
+
+# # Application definition
+
+# INSTALLED_APPS = [
+       
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     'app',  # Enable the inner app ,
+#     'id_manager',
+#     'reporting',
+#     'eod',
+#     'clusters'
+# ]
+
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
+
+# ROOT_URLCONF = 'core.urls'
+# LOGIN_REDIRECT_URL = "home"   # Route defined in app/urls.py
+# LOGOUT_REDIRECT_URL = "home"  # Route defined in app/urls.py
+# TEMPLATE_DIR = os.path.join(BASE_DIR, "core/templates")  # ROOT dir for templates
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'mail.crazyparrot.app'#'indexglobalnetwork.com'
+# EMAIL_HOST_USER = 'smtp@crazyparrot.app'#'smtp@indexglobalnetwork.com'
+# EMAIL_HOST_PASSWORD = 'Fx~dc827'#'*Ks)nvAhNGte'
+# EMAIL_PORT = 465
+# EMAIL_USE_SSL  = True
+
+
+
+# DATE_INPUT_FORMATS = [
+#     '%Y-%m-%d %H:%M:%S',     # '2006-10-25 14:30:59'
+#     '%Y-%m-%d %H:%M:%S.%f',  # '2006-10-25 14:30:59.000200'
+#     '%Y-%m-%d %H:%M',        # '2006-10-25 14:30'
+#     '%Y-%m-%d',              # '2006-10-25'
+#     '%m/%d/%Y %H:%M:%S',     # '10/25/2006 14:30:59'
+#     '%m/%d/%Y %H:%M:%S.%f',  # '10/25/2006 14:30:59.000200'
+#     '%m/%d/%Y %H:%M',        # '10/25/2006 14:30'
+#     '%m/%d/%Y',              # '10/25/2006'
+#     '%m/%d/%y %H:%M:%S',     # '10/25/06 14:30:59'
+#     '%m/%d/%y %H:%M:%S.%f',  # '10/25/06 14:30:59.000200'
+#     '%m/%d/%y %H:%M',        # '10/25/06 14:30'
+#     '%m/%d/%y',              # '10/25/06'
+# ]
+
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [TEMPLATE_DIR],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
+# WSGI_APPLICATION = 'core.wsgi.application'
+
+# # Database
+# # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+# DATABASES = {    
+#       'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'nato_db',
+#         'USER': 'root',
+#         'PASSWORD': '4dm1n',
+#         'HOST': 'localhost',
+#         'PORT': '3306'
+#     }
+# }
+
+# # Password validation
+# # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
+
+
+# # Internationalization
+# # https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+# LANGUAGE_CODE = 'en-us'
+
+# TIME_ZONE = 'Africa/Nairobi'
+
+# USE_I18N = True
+
+# USE_L10N = True
+
+# USE_TZ = True
+
+# #############################################################
+# # SRC: https://devcenter.heroku.com/articles/django-assets
+
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/1.9/howto/static-files/
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'core/static'),
+# )
+# #############################################################
+# #############################################################
 # -*- encoding: utf-8 -*-
 
 import os
@@ -13,15 +170,16 @@ PROJECT_DIR = Path(__file__).parent
 SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False)
+DEBUG = config('DEBUG', default=True)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '102.83.76.169', config('SERVER', default='127.0.0.1')]
+
+ALLOWED_HOSTS = ['www.natolive.nwf-ug.com', '45.56.127.148']
+
 
 # Application definition
 
 INSTALLED_APPS = [
-       
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -99,9 +257,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {    
       'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nato_db',
-        'USER': 'root',
-        'PASSWORD': '4dm1n',
+        'NAME': 'nwfugcom_natolive',
+        'USER': 'nwfugcom_dbadmin',
+        'PASSWORD': 'eWH*c*#%rI9d',
         'HOST': 'localhost',
         'PORT': '3306'
     }
@@ -155,3 +313,4 @@ STATICFILES_DIRS = (
 )
 #############################################################
 #############################################################
+
